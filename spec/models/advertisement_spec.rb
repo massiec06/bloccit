850780a5998e600dc3a6f64c832d5dafe6f4293d
Chris Massie <massiec06@gmail.com>
2017-08-11 11:22:15 -0400

    assignment 18 CRUD: in progress

---
+++ spec/models/advertisement_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Advertisement, type: :model do
+  let (:advertisement) { Advertisement.create! }
+
+  describe "attributes" do
+    it "should respond to title" do
+      expect(advertisement).to respond_to(:title)
+    end
+
+    it "should respond to copy" do
+      expect(advertisement).to respond_to(:copy)
+    end
+
+    it "should respond to price" do
+      expect(advertiesment).to respond_to(:copy)
+    end
+  end
+end
