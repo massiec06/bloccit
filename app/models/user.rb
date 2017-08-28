6b06b5f1162a3b4154d7743c271bbef0f1d50f6c
Chris Massie <massiec06@gmail.com>
2017-08-28 11:28:26 -0400

    asignment 23 Authent: new complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   before_save :proper_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { password_digest.nil? }
   validates :password, length: { minimum: 6 }, allow_blank: true
   validates :email,
             presence: true,
