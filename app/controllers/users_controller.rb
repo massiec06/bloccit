6431f867adae7116ee2ab901f885a045230fae96
Chris Massie <massiec06@gmail.com>
2017-09-05 08:25:32 -0400

    assignment 24 user: completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,19 @@ class UsersController < ApplicationController
   end
 
   def confirm
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    # @user = params[:user]
+    # @user.name = params[:user][:name]
+    # @user.email = params[:user][:email]
+    # @user.password = params[:user][:password]
+    # @user.password_confirmation = params[:user][:password_confirmation]
   end
 
   def create
     @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    @user.password_confirmation = params[:password_confirmation]
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
