4bd19b8d219efab7d476abf7f53d3b65b2cc7e33
Chris Massie <massiec06@gmail.com>
2017-09-11 12:04:34 -0400

    assignment 31 Public: completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  def has_posts_or_comments(user)
+    ret = ""
+    if user.posts.present?
+      ret += "<h2>Posts</h2>"
+      ret += "#{render user.posts}"
+    else
+      ret += "#{user.name} has not submitted any posts yet."
+    end
+
+    if user.comments.present?
+      ret += "<h2>Comments</h2>"
+      ret += "#{render user.comments}"
+    else
+      ret += "#{user.name} has not submitted any comments yet."
+    end
+
+    ret.html_safe
+  end
 end
